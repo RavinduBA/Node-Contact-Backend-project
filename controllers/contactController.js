9c6049ca2b76dbb9d161b2d7be0c907be29546cc
RavinduBA <e19004@eng.pdn.ac.lk>
2025-02-03 12:02:40 +0530

    bug fix ```

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -79,7 +79,7 @@ const deleteContact = asyncHandler(async (req, res) =>{
         res.status(403);
         throw new Error("User dont have permission to delete other user contacts !");
     }
-    await Contact.deleteOne();
+    await Contact.deleteOne({_id: req.params.id});
     res.status(200).json(contact);
 });
 
